e828e1baca3f8b962c3ec8a362430f1cfd1aba7b
rizwan230698 <rizwananwar230698@gmail.com>
2020-01-18 21:29:57 +0530

    remove logger from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = [logger];
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(
   rootReducer,
