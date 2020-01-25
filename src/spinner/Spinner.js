277f588222b1d05134abec557b198e6bff7eea80
rizwan230698 <rizwananwar230698@gmail.com>
2020-01-25 19:09:02 +0530

    improving className of elements

--- src/spinner/Spinner.js
+++ src/spinner/Spinner.js
@@ -3,7 +3,7 @@ import "./Spinner.scss";
 const spinner = ({ className, text }) => (
   <div className={`${className} wrapper`}>
     <div className="loading-spinner"></div>
-    <span className="signingUp">{text}</span>
+    <span className="text">{text}</span>
   </div>
 );
 export default spinner;
